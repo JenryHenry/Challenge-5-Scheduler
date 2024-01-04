728faf1fb72d57af9346d347ce1b2fb7543dfc62
Lizard <josephmhenry42@gmail.com>
2024-01-03 23:08:06 -0600

    add color code and current day functionality

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var saveButtons = $(".saveBtn");
-var textArea = $("");
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -14,22 +13,36 @@ $(function () {
     var btn = $(event.target);
     // determine the textarea name from data attributes
     var selectHour = btn.data().hour;
-    console.log(btn);
     // get a handle on the textarea using a name query selector
     var textVal = $("textarea[name=" + selectHour + "]").val();
     // save the value of the textarea to local storage using the same name.
     localStorage.setItem(selectHour, textVal);
-    console.log(selectHour);
     // console.log(selHour);
     // key name will be hour-#
   });
   // set the textarea values
   for (var hour = 9; hour <= 17; hour++) {
+    var timeNow = dayjs().hour();
     var selHour = "hour-" + hour;
     var textValue = localStorage.getItem(selHour);
     $("textarea[name=" + selHour + "]").val(textValue);
+    console.log(hour);
+    // console.log(timeNow);
+    if (hour == timeNow) {
+      $("textarea[name=" + selHour + "]").addClass("present");
+    } else if (hour < timeNow) {
+      $("textarea[name=" + selHour + "]").addClass("past");
+    } else if (hour > timeNow) {
+      $("textarea[name=" + selHour + "]").addClass("future");
+    }
   }
 
+  var currentDate = dayjs();
+  console.log(currentDate);
+  $("#currentDay").text(currentDate.format("[Today is] MMM D, YYYY"));
+  // var timeNow = dayjs().hour();
+
+  // console.log(timeNow);
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
